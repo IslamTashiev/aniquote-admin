f8156a9b26de9c5e682a61dde7596fcfc7ce59c9
Islam Tashiev <ueaten04@gmail.com>
2023-12-24 01:16:33 +0600

    add Create new admin modal

---
+++ src/pages/Home/components/AdminForm.tsx
@@ -0,0 +1,78 @@
+import React, { FC, useState } from "react";
+
+interface AdminFormProps {
+	onSubmit: (formData: FormData) => void;
+}
+
+export interface FormData {
+	fullName: string;
+	email: string;
+	password: string;
+	role: string;
+}
+
+const AdminForm: FC<AdminFormProps> = ({ onSubmit }) => {
+	const [formData, setFormData] = useState<FormData>({
+		fullName: "",
+		email: "",
+		password: "",
+		role: "ADMIN",
+	});
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onSubmit(formData);
+		setFormData({
+			fullName: "",
+			email: "",
+			password: "",
+			role: "",
+		});
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className='mx-auto bg-white'>
+			<div className='mb-4'>
+				<label htmlFor='fullName' className='block mb-1 font-medium'>
+					Full Name:
+				</label>
+				<input type='text' id='fullName' name='fullName' value={formData.fullName} onChange={handleChange} className='w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-blue-500' />
+			</div>
+			<div className='mb-4'>
+				<label htmlFor='email' className='block mb-1 font-medium'>
+					Email:
+				</label>
+				<input type='email' id='email' name='email' value={formData.email} onChange={handleChange} className='w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-blue-500' />
+			</div>
+			<div className='mb-4'>
+				<label htmlFor='password' className='block mb-1 font-medium'>
+					Password:
+				</label>
+				<input type='password' id='password' name='password' value={formData.password} onChange={handleChange} className='w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-blue-500' />
+			</div>
+			<div className='mb-6'>
+				<label htmlFor='role' className='block mb-1 font-medium'>
+					Role:
+				</label>
+				<select id='role' name='role' value={formData.role} onChange={handleChange} className='w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-blue-500'>
+					<option value='ADMIN'>Admin</option>
+					<option value='CEO'>CEO</option>
+					<option value='MANAGER'>Manager</option>
+				</select>
+			</div>
+			<button type='submit' className='w-full bg-blue-500 text-white font-semibold rounded-md py-2'>
+				Create Admin
+			</button>
+		</form>
+	);
+};
+
+export default AdminForm;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
+import React, { useState, useEffect } from "react";
 import { useAppStore } from "@store/appStore";
-import AdminCard from "./components/AdminCard";
+import Modal from "@components/Modal";
 import AddIcon from "@assets/add-admin.svg";
 import AdminsList from "./components/AdminsList";
-import { useEffect } from "react";
+import AdminForm, { FormData } from "./components/AdminForm";
 
 const Home = () => {
 	const { adminsList, getAdminsList } = useAppStore((state) => state);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleSubmit = (formData: FormData) => {
+		setIsModalOpen(false);
+	};
 
 	useEffect(() => {
 		getAdminsList();
@@ -16,11 +22,14 @@ const Home = () => {
 			<h2 className='text-2xl'>Workspace admins</h2>
 			<div className='flex gap-2 mt-2 flex-wrap'>
 				<AdminsList adminsList={adminsList} />
-				<div className='max-w-44 h-56 flex flex-col justify-center items-center border border-blue-600 rounded-lg w-full cursor-pointer'>
+				<div className='max-w-44 h-56 flex flex-col justify-center items-center border border-blue-600 rounded-lg w-full cursor-pointer' onClick={() => setIsModalOpen(true)}>
 					<AddIcon />
 					<p className='text-blue-600'>Add admin</p>
 				</div>
 			</div>
+			<Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title='Create a new admin'>
+				<AdminForm onSubmit={handleSubmit} />
+			</Modal>
 		</div>
 	);
 };
