b9804ba993d1194ea1178e2c45a93b8be61e56cd
Islam Tashiev <ueaten04@gmail.com>
2023-12-23 17:41:06 +0600

    change default svg icons to react-component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import AdminCard from "./components/AdminCard";
+import AddIcon from "@assets/add-admin.svg";
 
 const Home = () => {
 	return (
@@ -7,10 +8,7 @@ const Home = () => {
 			<div className='flex gap-2 mt-2 flex-wrap'>
 				<AdminCard />
 				<div className='max-w-44 h-56 flex flex-col justify-center items-center border border-blue-600 rounded-lg w-full cursor-pointer'>
-					<svg width='33' height='32' viewBox='0 0 33 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
-						<path d='M5.5 16H27.5' stroke='#0832DE' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
-						<path d='M16.5 5V27' stroke='#0832DE' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
-					</svg>
+					<AddIcon />
 					<p className='text-blue-600'>Add admin</p>
 				</div>
 			</div>
